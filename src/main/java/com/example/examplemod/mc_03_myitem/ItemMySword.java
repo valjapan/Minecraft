b915b1f0983ae755f7a3f341e56207a99d807a9c
yukiyuki961 <yukinabe3561@icloud.com>
2019-04-08 16:43:52 +0900

    TF03

--- src/main/java/com/example/examplemod/mc_03_myitem/ItemMySword.java
+++ src/main/java/com/example/examplemod/mc_03_myitem/ItemMySword.java
@@ -4,10 +4,12 @@ import com.example.examplemod.ExampleMod;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.util.EnumHelper;
 
 public class ItemMySword extends ItemSword {
@@ -26,7 +28,32 @@ public class ItemMySword extends ItemSword {
         if (!(attacker instanceof EntityPlayer)) {
             return true;
         }
+
+        BlockPos pos = new BlockPos(target.posX + 1, target.posY, target.posZ);
+        BlockPos pos1 = new BlockPos(target.posX - 1, target.posY, target.posZ);
+        BlockPos pos2 = new BlockPos(target.posX, target.posY, target.posZ + 1);
+        BlockPos pos3 = new BlockPos(target.posX, target.posY, target.posZ - 1);
+        BlockPos pos4 = new BlockPos(target.posX, target.posY - 1, target.posZ);
+        BlockPos pos5 = new BlockPos(target.posX + 1, target.posY + 20, target.posZ);
+        BlockPos pos6 = new BlockPos(target.posX - 1, target.posY + 20, target.posZ);
+        BlockPos pos7 = new BlockPos(target.posX, target.posY + 20, target.posZ + 1);
+        BlockPos pos8 = new BlockPos(target.posX, target.posY + 20, target.posZ - 1);
+
+        target.worldObj.setBlockState(pos, ExampleMod.blockMyBlock.getDefaultState());
+        target.worldObj.setBlockState(pos1, Blocks.GLASS.getDefaultState());
+        target.worldObj.setBlockState(pos2, Blocks.GLASS.getDefaultState());
+        target.worldObj.setBlockState(pos3, Blocks.GLASS.getDefaultState());
+        target.worldObj.setBlockState(pos4, Blocks.SLIME_BLOCK.getDefaultState());
+        target.worldObj.setBlockState(pos5, Blocks.GLASS.getDefaultState());
+        target.worldObj.setBlockState(pos6, Blocks.GLASS.getDefaultState());
+        target.worldObj.setBlockState(pos7, Blocks.GLASS.getDefaultState());
+        target.worldObj.setBlockState(pos8, Blocks.GLASS.getDefaultState());
+
         target.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 1200, 0));
+        target.addPotionEffect(new PotionEffect(MobEffects.HEALTH_BOOST, 1200, 0));
+//        target.setFire(600);
+        target.setPosition(target.posX, target.posY + 20, target.posZ);
+
         return true;
     }
 }
